c9860a228465ae9403cd8515d2d1c130f5d83202
oscar <oscar@DESKTOP-2QNPKAG>
2024-02-17 10:37:26 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/equipoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equipoController : ControllerBase
+    {
+
+        private readonly equiposContext _equiposContexto;
+
+        public equipoController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+
+    }
+}
