23d8c8e044643798796f35e810955ea365eea314
oscar <oscar.martz511@gmail.com>
2024-02-17 11:09:03 -0600

    Se han agregado los metodos para buscar por id y para buscar por descripción

--- WebApplication1/Controllers/equipoController.cs
+++ WebApplication1/Controllers/equipoController.cs
@@ -16,6 +16,7 @@ namespace WebApplication1.Controllers
             _equiposContexto = equiposContexto;
         }
 
+        //Método para leer los registros de la tabla de la base de datos
         [HttpGet]
         [Route("GetAll")]
 
@@ -30,6 +31,41 @@ namespace WebApplication1.Controllers
             }
             return Ok(listadoEquipo);
         }
+        //Fin del método
+
+        //Método para buscar por ID
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id) 
+        {
+            //El "?" sirve para que acepte nulos
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+        //Fin del método
+
+        //Método para buscar por descripción
+        [HttpGet]
+        [Route("Find/{filtro}")]
+        public IActionResult FindByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+        //Fin del método
 
     }
 }
